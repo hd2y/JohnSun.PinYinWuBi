fcdda17c237c8689b9568b52cd056db005d964f6
Sun John <hd2y@outlook.com>
2019-02-27 17:14:36 +0800

    提交

---
+++ JohnSun.PinYinWuBi.ConsoleTests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace JohnSun.PinYinWuBi.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "南京市长江大桥(Nanjing Yangtze River Bridge)";
+            Test(text);
+            Console.ReadKey();
+        }
+
+        public static void Test(string text)
+        {
+            Console.WriteLine($"测试输出：{text}");
+            Console.WriteLine("==============测试获取汉字信息==============");
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    string[] temp = PinYinWuBiHelper.GetCharInfo(text, i);
+                    if (temp == null)
+                    {
+                        Console.WriteLine($"【{text.Substring(i, 1)}】非汉字或非帮助类收录汉字。");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"【{text.Substring(i, 1)}】********\r\n五笔:{temp[0]}\r\n仓颉:{temp[1]}\r\n郑码:{temp[2]}\r\n笔顺:{temp[3].Replace("1", "一").Replace("2", "丨").Replace("3", "ノ").Replace("4", "丶").Replace("5", "フ")}\r\n拼音:{string.Join(";", temp.ToList().GetRange(4, temp.Length - 4))}");
+                    }
+                }
+            }
+            Console.WriteLine("==============获取拼音首码信息==============");
+            {
+                Console.WriteLine(PinYinWuBiHelper.GetPinYinFirstCode(text));
+            }
+            Console.WriteLine("==============获取全部拼音首码==============");
+            {
+                string[] temp = PinYinWuBiHelper.GetAllPinYinFirstCode(text);
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    Console.WriteLine(temp[i]);
+                }
+            }
+            Console.WriteLine("==============获取全部五笔首码==============");
+            {
+                Console.WriteLine(PinYinWuBiHelper.GetWuBiFirstCode(text));
+            }
+        }
+    }
+}
---
+++ JohnSun.PinYinWuBi/PinYinWuBiHelper.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace JohnSun.PinYinWuBi
+{
+    /// <summary>
+    /// 获取拼音五笔工具类
+    /// </summary>
+    public class PinYinWuBiHelper
+    {
+        private static Dictionary<int, string[]> _data { get; } = ReadData();
+
+        private static Dictionary<int, string[]> ReadData()
+        {
+            Dictionary<int, string[]> dict = new Dictionary<int, string[]>();
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("JohnSun.PinYinWuBi.DATA"))
+            {
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                string[] lines = Encoding.UTF8.GetString(bytes).Split('\n');
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] temp = lines[i].Split('|');
+                    dict[int.Parse(temp[0])] = temp.ToList().GetRange(1, temp.Length - 1).ToArray();
+                }
+            }
+            return dict;
+        }
+
+        /// <summary>
+        /// 获取指定字符的信息，返回一个数组：
+        /// 索引0：五笔
+        /// 索引1：仓颉
+        /// 索引2：郑码
+        /// 索引3：笔顺
+        /// 索引4+：拼音
+        /// </summary>
+        /// <param name="input">包含字符或代理项对的字符串。</param>
+        /// <param name="index">字符或代理项对在 s 中的索引位置。</param>
+        /// <returns>指定字符的信息</returns>
+        public static string[] GetCharInfo(string input, int index)
+        {
+            int key = char.ConvertToUtf32(input, index);
+            if (_data.ContainsKey(key))
+                return _data[key];
+            return null;
+        }
+
+        /// <summary>
+        /// 获取字符串拼音首码
+        /// </summary>
+        /// <param name="input">输入内容</param>
+        /// <returns>所有可能的拼音首码</returns>
+        public static string[] GetAllPinYinFirstCode(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return new string[] { "" };
+            StringBuilder builder = new StringBuilder();
+
+            List<StringBuilder> builders = new List<StringBuilder>() { new StringBuilder() };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int key = char.ConvertToUtf32(input, i);
+                if (key >= 48 && key <= 57 || key >= 65 && key <= 90)
+                {
+                    foreach (StringBuilder item in builders)
+                    {
+                        item.Append(input.Substring(i, 1));
+                    }
+                }
+                else if (key >= 97 && key <= 122)
+                {
+                    foreach (StringBuilder item in builders)
+                    {
+                        item.Append(input.Substring(i, 1).ToUpper());
+                    }
+                }
+                else
+                {
+                    string[] charInfo = GetCharInfo(input, i);
+                    if (charInfo != null)
+                    {
+                        List<string> temp = new List<string>();
+                        for (int j = 4; j < charInfo.Length; j++)
+                        {
+                            if (!string.IsNullOrEmpty(charInfo[j]) && !temp.Contains(charInfo[j].Substring(0, 1).ToUpper()))
+                            {
+                                temp.Add(charInfo[j].Substring(0, 1).ToUpper());
+                            }
+                        }
+                        if (temp.Count > 0)
+                        {
+                            List<StringBuilder> tempBuilders = new List<StringBuilder>();
+                            for (int j = 0; j < builders.Count; j++)
+                            {
+                                for (int k = 0; k < temp.Count; k++)
+                                {
+                                    tempBuilders.Add(new StringBuilder(builders[j].ToString()).Append(temp[k]));
+                                }
+                            }
+                            builders = tempBuilders;
+                        }
+                    }
+                }
+            }
+
+            List<string> pinyins = new List<string>();
+            builders.ForEach(b => pinyins.Add(b.ToString()));
+            return pinyins.Distinct().ToArray();
+        }
+
+        /// <summary>
+        /// 获取字符串拼音首码
+        /// </summary>
+        /// <param name="input">输入内容</param>
+        /// <returns>获取一个拼音首码</returns>
+        public static string GetPinYinFirstCode(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int key = char.ConvertToUtf32(input, i);
+                if (key >= 48 && key <= 57 || key >= 65 && key <= 90)
+                {
+                    builder.Append(input.Substring(i, 1));
+                }
+                else if (key >= 97 && key <= 122)
+                {
+                    builder.Append(input.Substring(i, 1).ToUpper());
+                }
+                else
+                {
+                    string[] charInfo = GetCharInfo(input, i);
+                    if (charInfo != null && !string.IsNullOrEmpty(charInfo[4]))
+                    {
+                        builder.Append(charInfo[4].Substring(0, 1).ToUpper());
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// 获取字符串的五笔首码
+        /// </summary>
+        /// <param name="input">输入内容</param>
+        /// <returns>该内容的五笔首码</returns>
+        public static string GetWuBiFirstCode(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int key = char.ConvertToUtf32(input, i);
+                if (key >= 48 && key <= 57 || key >= 65 && key <= 90)
+                {
+                    builder.Append(input.Substring(i, 1));
+                }
+                else if (key >= 97 && key <= 122)
+                {
+                    builder.Append(input.Substring(i, 1).ToUpper());
+                }
+                else
+                {
+                    string[] charInfo = GetCharInfo(input, i);
+                    if (charInfo != null && !string.IsNullOrEmpty(charInfo[0]))
+                    {
+                        builder.Append(charInfo[0].Substring(0, 1).ToUpper());
+                    }
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
